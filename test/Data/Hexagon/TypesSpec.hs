563229fb34ee063770df2e901ad070b886b92777
Markus Barenhoff <mbarenh@alios.org>
2015-09-16 17:47:34 +0200

    added offset coordinate related addresses

--- test/Data/Hexagon/TypesSpec.hs
+++ test/Data/Hexagon/TypesSpec.hs
@@ -24,6 +24,42 @@ spec = do
       it "_AxialOffsetIso is isomorph with OffsetEvenR" $ property $ prop_axial_offsetEvenR_iso
       it "_AxialOffsetIso is isomorph with OffsetOddQ" $ property $ prop_axial_offsetOddQ_iso
       it "_AxialOffsetIso is isomorph with OffsetOddR" $ property $ prop_axial_offsetOddR_iso
+    context "OffsetEvenQ" $ do
+      it "_OffsetCoordinate is isomproph" $ property $ prop_offset_eq_iso
+      it "_AxialOffsetIso is isomorph" $ property $ prop_offsetEvenQ_axial_iso
+      it "_AxialOffsetIso is isomorph" $ property $ prop_offsetEvenQ_cube_iso
+
+    context "OffsetEvenR" $ do
+      it "_OffsetCoordinate is isomproph" $ property $ prop_offset_er_iso
+      it "_AxialOffsetIso is isomorph" $ property $ prop_offsetEvenR_axial_iso
+      it "_AxialOffsetIso is isomorph" $ property $ prop_offsetEvenR_cube_iso
+
+
+prop_offset_eq_iso :: OffsetEvenQ Int -> Bool
+prop_offset_eq_iso c = c == (view (_OffsetCoordinate . from _OffsetCoordinate ) c)
+
+prop_offsetEvenQ_axial_iso :: OffsetEvenQ Int -> Bool
+prop_offsetEvenQ_axial_iso c =
+  c == (view (from _AxialOffsetIso . _AxialOffsetIso) c)
+
+
+prop_offsetEvenQ_cube_iso :: OffsetEvenQ Int -> Bool
+prop_offsetEvenQ_cube_iso c =
+  c == (view (from _CubeOffsetIso . _CubeOffsetIso) c)
+
+
+prop_offset_er_iso :: OffsetEvenR Int -> Bool
+prop_offset_er_iso c = c == (view (_OffsetCoordinate . from _OffsetCoordinate ) c)
+
+prop_offsetEvenR_axial_iso :: OffsetEvenR Int -> Bool
+prop_offsetEvenR_axial_iso c =
+  c == (view (from _AxialOffsetIso . _AxialOffsetIso) c)
+
+prop_offsetEvenR_cube_iso :: OffsetEvenR Int -> Bool
+prop_offsetEvenR_cube_iso c =
+  c == (view (from _CubeOffsetIso . _CubeOffsetIso) c)
+
+
 
 prop_cube_iso :: CubeCoordinate Int -> Bool
 prop_cube_iso c = c == (view (_CubeCoordinate . from _CubeCoordinate) c)
