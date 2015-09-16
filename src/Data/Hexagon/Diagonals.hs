b9697a1cef52414be62f096d578433dd70d4c8c6
Markus Barenhoff <mbarenh@alios.org>
2015-09-16 08:44:48 +0200

    added diagonal function

--- src/Data/Hexagon/Diagonals.hs
+++ src/Data/Hexagon/Diagonals.hs
@@ -3,7 +3,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE Safe                  #-}
 
-module Data.Hexagon.Diagonals (diagonals) where
+module Data.Hexagon.Diagonals (diagonals, diagonal) where
 
 import           Control.Lens
 import           Data.Bits
@@ -14,6 +14,8 @@ diagonals :: (HexCoordinate t a, Integral a, Bits a) => t a -> [ t a ]
 diagonals c = fmap (view _CoordinateIso) $
               cubeDiagonals (c ^. re _CoordinateCubeIso)
 
+diagonal :: (HexCoordinate t a, Integral a, Bits a) => Direction -> t a -> t a
+diagonal d = (flip (!!)) (fromEnum d) . diagonals
 
 cubeDiagonals :: Num t => CubeCoordinate t -> [CubeCoordinate t]
 cubeDiagonals c =
