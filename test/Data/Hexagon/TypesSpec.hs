9969466d1714459b2f0d5583300463b448c28ace
Markus Barenhoff <mbarenh@alios.org>
2015-09-16 13:43:15 +0200

    added axial tests

--- test/Data/Hexagon/TypesSpec.hs
+++ test/Data/Hexagon/TypesSpec.hs
@@ -17,13 +17,27 @@ spec = do
       it "_CubeOffsetIso is isomorph with OffsetEvenR" $ property $ prop_cube_offsetEvenR_iso
       it "_CubeOffsetIso is isomorph with OffsetOddQ" $ property $ prop_cube_offsetOddQ_iso
       it "_CubeOffsetIso is isomorph with OffsetOddR" $ property $ prop_cube_offsetOddR_iso
-      
+    context "AxialCoordinate" $ do
+      it "_AxialCoordinate is isomorph" $ property $ prop_axial_iso
+      it "_CubeAxialIso is isomorph" $ property $ prop_axial_cube_iso
+      it "_AxialOffsetIso is isomorph with OffsetEvenQ" $ property $ prop_axial_offsetEvenQ_iso
+      it "_AxialOffsetIso is isomorph with OffsetEvenR" $ property $ prop_axial_offsetEvenR_iso
+      it "_AxialOffsetIso is isomorph with OffsetOddQ" $ property $ prop_axial_offsetOddQ_iso
+      it "_AxialOffsetIso is isomorph with OffsetOddR" $ property $ prop_axial_offsetOddR_iso
 
 prop_cube_iso :: CubeCoordinate Int -> Bool
 prop_cube_iso c = c == (view (_CubeCoordinate . from _CubeCoordinate) c)
 
+prop_axial_iso :: AxialCoordinate Int -> Bool
+prop_axial_iso c = c == (view (_AxialCoordinate . from _AxialCoordinate) c)
+
 prop_cube_axial_iso :: CubeCoordinate Int -> Bool
 prop_cube_axial_iso c = c == (view (_CubeAxialIso . from _CubeAxialIso) c)
+
+prop_axial_cube_iso :: AxialCoordinate Int -> Bool
+prop_axial_cube_iso c = c == (view (from _CubeAxialIso . _CubeAxialIso) c)
+
+
 
 prop_cube_offsetEvenQ_iso :: CubeCoordinate Int -> Bool
 prop_cube_offsetEvenQ_iso c =
@@ -52,7 +66,32 @@ prop_cube_offsetOddR_iso c =
       c' = view (_CubeOffsetIso) c
   in c == (view (from _CubeOffsetIso) c')
 
+prop_axial_offsetEvenQ_iso :: AxialCoordinate Int -> Bool
+prop_axial_offsetEvenQ_iso c =
+  let c' :: OffsetEvenQ Int
+      c' = view (_AxialOffsetIso) c
+  in c == (view (from _AxialOffsetIso) c')
 
+
+prop_axial_offsetEvenR_iso :: AxialCoordinate Int -> Bool
+prop_axial_offsetEvenR_iso c =
+  let c' :: OffsetEvenR Int
+      c' = view (_AxialOffsetIso) c
+  in c == (view (from _AxialOffsetIso) c')
+
+
+prop_axial_offsetOddQ_iso :: AxialCoordinate Int -> Bool
+prop_axial_offsetOddQ_iso c =
+  let c' :: OffsetOddQ Int
+      c' = view (_AxialOffsetIso) c
+  in c == (view (from _AxialOffsetIso) c')
+
+
+prop_axial_offsetOddR_iso :: AxialCoordinate Int -> Bool
+prop_axial_offsetOddR_iso c =
+  let c' :: OffsetOddR Int
+      c' = view (_AxialOffsetIso) c
+  in c == (view (from _AxialOffsetIso) c')
 
 
 main :: IO ()
