1bc4c9e15491be634a95199888e0a72ebace5f21
Markus Barenhoff <mbarenh@alios.org>
2015-09-16 13:19:18 +0200

    added type spec tests

--- test/Data/Hexagon/TypesSpec.hs
+++ test/Data/Hexagon/TypesSpec.hs
@@ -1,14 +1,58 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Data.Hexagon.TypesSpec where
 
 import SpecHelper
+import Control.Lens
 
 
+  
 spec :: Spec
 spec = do
   describe "Data.Hexagon.Types" $ do
-    context "context" $ do
-      it "does something" $ do
-        True `shouldBe` True
+    context "CubeCoordinate" $ do
+      it "_CubeCoordinate is isomorph" $ property $ prop_cube_iso
+      it "_CubeAxialIso is isomorph" $ property $ prop_cube_axial_iso
+      it "_CubeOffsetIso is isomorph with OffsetEvenQ" $ property $ prop_cube_offsetEvenQ_iso
+      it "_CubeOffsetIso is isomorph with OffsetEvenR" $ property $ prop_cube_offsetEvenR_iso
+      it "_CubeOffsetIso is isomorph with OffsetOddQ" $ property $ prop_cube_offsetOddQ_iso
+      it "_CubeOffsetIso is isomorph with OffsetOddR" $ property $ prop_cube_offsetOddR_iso
+      
+
+prop_cube_iso :: CubeCoordinate Int -> Bool
+prop_cube_iso c = c == (view (_CubeCoordinate . from _CubeCoordinate) c)
+
+prop_cube_axial_iso :: CubeCoordinate Int -> Bool
+prop_cube_axial_iso c = c == (view (_CubeAxialIso . from _CubeAxialIso) c)
+
+prop_cube_offsetEvenQ_iso :: CubeCoordinate Int -> Bool
+prop_cube_offsetEvenQ_iso c =
+  let c' :: OffsetEvenQ Int
+      c' = view (_CubeOffsetIso) c
+  in c == (view (from _CubeOffsetIso) c')
+
+
+prop_cube_offsetEvenR_iso :: CubeCoordinate Int -> Bool
+prop_cube_offsetEvenR_iso c =
+  let c' :: OffsetEvenR Int
+      c' = view (_CubeOffsetIso) c
+  in c == (view (from _CubeOffsetIso) c')
+
+
+prop_cube_offsetOddQ_iso :: CubeCoordinate Int -> Bool
+prop_cube_offsetOddQ_iso c =
+  let c' :: OffsetOddQ Int
+      c' = view (_CubeOffsetIso) c
+  in c == (view (from _CubeOffsetIso) c')
+
+
+prop_cube_offsetOddR_iso :: CubeCoordinate Int -> Bool
+prop_cube_offsetOddR_iso c =
+  let c' :: OffsetOddR Int
+      c' = view (_CubeOffsetIso) c
+  in c == (view (from _CubeOffsetIso) c')
+
+
 
 
 main :: IO ()
--- test/SpecHelper.hs
+++ test/SpecHelper.hs
@@ -1,7 +1,34 @@
 module SpecHelper
        ( module Test.Hspec
+       , module Test.QuickCheck
        , module Data.Hexagon
        ) where
 
 import Test.Hspec
+import Test.QuickCheck
 import Data.Hexagon
+
+import Control.Lens
+
+
+instance (Num a, Arbitrary a) => Arbitrary (CubeCoordinate a) where
+  arbitrary = do
+    x <- arbitrary
+    z <- arbitrary
+    let y = -x-z
+    return $ _CubeCoordinate # (x,y,z)
+
+instance (Arbitrary a) => Arbitrary (AxialCoordinate a) where
+  arbitrary = fmap (review _AxialCoordinate) arbitrary 
+
+instance (Arbitrary a) => Arbitrary (OffsetEvenQ a) where
+  arbitrary = fmap (review _OffsetEvenQ) arbitrary 
+
+instance (Arbitrary a) => Arbitrary (OffsetEvenR a) where
+  arbitrary = fmap (review _OffsetEvenR) arbitrary 
+
+instance (Arbitrary a) => Arbitrary (OffsetOddQ a) where
+  arbitrary = fmap (review _OffsetOddQ) arbitrary 
+
+instance (Arbitrary a) => Arbitrary (OffsetOddR a) where
+  arbitrary = fmap (review _OffsetOddR) arbitrary 
