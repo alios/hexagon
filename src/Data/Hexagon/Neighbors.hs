cbd56a756e0f6733bf137c18f13726a89f0d9002
Markus Barenhoff <mbarenh@alios.org>
2015-09-19 12:29:02 +0200

    neighbor related functions now work on Map from Direction to Coordinate

--- src/Data/Hexagon/Neighbors.hs
+++ src/Data/Hexagon/Neighbors.hs
@@ -9,116 +9,122 @@ import           Control.Lens.Operators
 import           Data.Hexagon.Types
 import           Data.List
 
-
+import           Data.Map               (Map)
+import qualified Data.Map               as Map
+import           Data.Maybe             (fromJust)
 -- | a 'HexCoordinate' which has neighbors.
 class (Integral a) => HasNeighbors (t :: * -> *) a where
   -- | get all 6 neighbors of a 'HexCoordinate'
-  directions :: t a -> [ t a ]
+  directions :: t a -> Map Direction (t a)
 
 
 -- | given a 'Direction' return that neighbor
 neighbor :: (HasNeighbors t a) => Direction -> t a -> t a
-neighbor d = (flip (!!)) (fromEnum d) . directions
+neighbor d = fromJust . Map.lookup d . directions
 
-
+-- | in case of a being a neighbor of b return 'Just' 'Direction' else 'Nothing'
 isNeighbor :: (HasNeighbors t a, Eq (t a)) =>  t a -> t a -> Maybe Direction
-isNeighbor a = fmap toEnum . elemIndex a . directions
+isNeighbor a = findMapKeyByValue a . directions
 
 
+findMapKeyByValue :: (Eq a) => a -> Map k a -> Maybe k
+findMapKeyByValue a = fmap fst . find ((==) a . snd) . Map.toList
 
-cubeDirections :: Num t => CubeCoordinate t -> [ CubeCoordinate t ]
-cubeDirections c =
-  [ c & cubeX +~ 1 & cubeY -~ 1
-  , c & cubeX +~ 1 & cubeZ -~ 1
-  , c & cubeY +~ 1 & cubeZ -~ 1
-  , c & cubeX -~ 1 & cubeY +~ 1
-  , c & cubeX -~ 1 & cubeZ +~ 1
-  , c & cubeY -~ 1 & cubeZ +~ 1
+
+cubeDirections :: Num t => CubeCoordinate t -> Map Direction (CubeCoordinate t)
+cubeDirections c = Map.fromList
+  [ (D0, c & cubeX +~ 1 & cubeY -~ 1)
+  , (D1, c & cubeX +~ 1 & cubeZ -~ 1)
+  , (D2, c & cubeY +~ 1 & cubeZ -~ 1)
+  , (D3, c & cubeX -~ 1 & cubeY +~ 1)
+  , (D4, c & cubeX -~ 1 & cubeZ +~ 1)
+  , (D5, c & cubeY -~ 1 & cubeZ +~ 1)
   ]
 
-axialDirections :: Num t => AxialCoordinate t -> [ AxialCoordinate t ]
-axialDirections c =
-  [ c & axialCol +~ 1
-  , c & axialCol +~ 1 & axialRow -~ 1
-  , c & axialRow -~ 1
-  , c & axialCol -~ 1
-  , c & axialCol -~ 1 & axialRow +~ 1
-  , c & axialRow +~ 1
+axialDirections :: Num t => AxialCoordinate t -> Map Direction (AxialCoordinate t)
+axialDirections c = Map.fromList
+  [ (D0, c & axialCol +~ 1)
+  , (D1, c & axialCol +~ 1 & axialRow -~ 1)
+  , (D2, c & axialRow -~ 1)
+  , (D3, c & axialCol -~ 1)
+  , (D4, c & axialCol -~ 1 & axialRow +~ 1)
+  , (D5, c & axialRow +~ 1)
   ]
 
 
-offsetEvenQDirections :: (Integral t) => OffsetEvenQ t -> [OffsetEvenQ t]
-offsetEvenQDirections c =
+offsetEvenQDirections :: (Integral t) =>
+                        OffsetEvenQ t -> Map Direction (OffsetEvenQ t)
+offsetEvenQDirections c = Map.fromList $
   if (even $ c ^. offsetCol)
-  then [ c & offsetCol +~ 1 & offsetRow +~ 1
-       , c & offsetCol +~ 1
-       , c & offsetRow -~ 1
-       , c & offsetCol -~ 1
-       , c & offsetCol -~ 1 & offsetRow +~ 1
-       , c & offsetRow +~ 1
+  then [ (D0, c & offsetCol +~ 1 & offsetRow +~ 1)
+       , (D1, c & offsetCol +~ 1)
+       , (D2, c & offsetRow -~ 1)
+       , (D3, c & offsetCol -~ 1)
+       , (D4, c & offsetCol -~ 1 & offsetRow +~ 1)
+       , (D5, c & offsetRow +~ 1)
        ]
-  else [ c & offsetCol +~ 1
-       , c & offsetCol +~ 1 & offsetRow -~ 1
-       , c & offsetRow -~ 1
-       , c & offsetCol -~ 1 & offsetRow -~ 1
-       , c & offsetCol -~ 1
-       , c & offsetRow +~ 1
+  else [ (D0, c & offsetCol +~ 1)
+       , (D1, c & offsetCol +~ 1 & offsetRow -~ 1)
+       , (D2, c & offsetRow -~ 1)
+       , (D3, c & offsetCol -~ 1 & offsetRow -~ 1)
+       , (D4, c & offsetCol -~ 1)
+       , (D5, c & offsetRow +~ 1)
        ]
 
 
-offsetOddQDirections :: (Integral t) => OffsetOddQ t -> [OffsetOddQ t]
-offsetOddQDirections c =
+offsetOddQDirections :: (Integral t) => OffsetOddQ t -> Map Direction (OffsetOddQ t)
+offsetOddQDirections c = Map.fromList $
   if (even $ c ^. offsetCol)
-  then [ c & offsetCol +~ 1
-       , c & offsetCol +~ 1 & offsetRow -~ 1
-       , c & offsetRow -~ 1
-       , c & offsetCol -~ 1 & offsetRow -~ 1
-       , c & offsetCol -~ 1
-       , c & offsetRow +~ 1
+  then [ (D0, c & offsetCol +~ 1)
+       , (D1, c & offsetCol +~ 1 & offsetRow -~ 1)
+       , (D2, c & offsetRow -~ 1)
+       , (D3, c & offsetCol -~ 1 & offsetRow -~ 1)
+       , (D4, c & offsetCol -~ 1)
+       , (D5, c & offsetRow +~ 1)
    ]
-  else [ c & offsetCol +~ 1 & offsetRow +~ 1
-       , c & offsetCol +~ 1
-       , c & offsetRow -~ 1
-       , c & offsetCol -~ 1
-       , c & offsetCol -~ 1 & offsetRow +~ 1
-       , c & offsetRow +~ 1
+  else [ (D0, c & offsetCol +~ 1 & offsetRow +~ 1)
+       , (D1, c & offsetCol +~ 1)
+       , (D2, c & offsetRow -~ 1)
+       , (D3, c & offsetCol -~ 1)
+       , (D4, c & offsetCol -~ 1 & offsetRow +~ 1)
+       , (D5, c & offsetRow +~ 1)
        ]
 
-offsetEvenRDirections :: (Integral t) => OffsetEvenR t -> [OffsetEvenR t]
-offsetEvenRDirections c =
+offsetEvenRDirections :: (Integral t) => OffsetEvenR t -> Map Direction (OffsetEvenR t)
+offsetEvenRDirections c = Map.fromList $
   if (even $ c ^. offsetRow)
-  then [ c & offsetCol +~ 1
-       , c & offsetCol +~ 1 & offsetRow -~ 1
-       , c &                  offsetRow -~ 1
-       , c & offsetCol -~ 1
-       , c &                  offsetRow +~ 1
-       , c & offsetCol +~ 1 & offsetRow +~ 1
+  then [ (D0, c & offsetCol +~ 1)
+       , (D1, c & offsetCol +~ 1 & offsetRow -~ 1)
+       , (D2, c &                  offsetRow -~ 1)
+       , (D3, c & offsetCol -~ 1)
+       , (D4, c &                  offsetRow +~ 1)
+       , (D5, c & offsetCol +~ 1 & offsetRow +~ 1)
        ]
-  else [ c & offsetCol +~ 1
-       , c &                  offsetRow -~ 1
-       , c & offsetCol -~ 1 & offsetRow -~ 1
-       , c & offsetCol -~ 1
-       , c & offsetCol -~ 1 & offsetRow +~ 1
-       , c &                  offsetRow +~ 1
+  else [ (D0, c & offsetCol +~ 1)
+       , (D1, c &                  offsetRow -~ 1)
+       , (D2, c & offsetCol -~ 1 & offsetRow -~ 1)
+       , (D3, c & offsetCol -~ 1)
+       , (D4, c & offsetCol -~ 1 & offsetRow +~ 1)
+       , (D5, c &                  offsetRow +~ 1)
        ]
 
 
-offsetOddRDirections :: (Integral t) => OffsetOddR t -> [OffsetOddR t]
-offsetOddRDirections c =
+offsetOddRDirections :: (Integral t) => OffsetOddR t -> Map Direction (OffsetOddR t)
+offsetOddRDirections c = Map.fromList $
   if (even $ c ^. offsetRow)
-  then [ c & offsetCol +~ 1
-       , c & offsetRow -~ 1
-       , c & offsetCol -~ 1 & offsetRow -~ 1
-       , c & offsetCol -~ 1
-       , c & offsetCol -~ 1 & offsetRow +~ 1
-       , c & offsetRow +~ 1
+  then [ (D0, c & offsetCol +~ 1)
+       , (D1, c & offsetRow -~ 1)
+       , (D2, c & offsetCol -~ 1 & offsetRow -~ 1)
+       , (D3, c & offsetCol -~ 1)
+       , (D4, c & offsetCol -~ 1 & offsetRow +~ 1)
+       , (D5, c & offsetRow +~ 1)
        ]
-  else [ c & offsetCol +~ 1
-       , c & offsetCol +~ 1 & offsetRow -~ 1
-       , c & offsetRow -~ 1
-       , c & offsetCol -~ 1
-       , c & offsetRow +~ 1
-       , c & offsetCol +~ 1 & offsetRow +~ 1
+  else [ (D0, c & offsetCol +~ 1)
+       , (D1, c & offsetCol +~ 1 & offsetRow -~ 1)
+       , (D2, c & offsetRow -~ 1)
+       , (D3, c & offsetCol -~ 1)
+       , (D4, c & offsetRow +~ 1)
+       , (D5, c & offsetCol +~ 1 & offsetRow +~ 1)
        ]
 
 
